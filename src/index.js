8e8530cca5e817c9207800780c5d26eb1fe6db72
RP <romainpareja@gmail.com>
2018-12-18 20:52:02 -0500

    k

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-import mediumZoom from 'medium-zoom'
+// import mediumZoom from 'medium-zoom'
+// mediumZoom(document.querySelector('#test'))
 
 
-mediumZoom(document.querySelector('#test'))
+let scrollpos = window.scrollY
+
+const header = document.querySelector(".header")
+const header_height = header.offsetHeight
+const navItems = document.querySelector(".nav__item")
+const navItems_height = navItems.offsetHeight;
+// console.log(navItems);
 
 
+
+const add_class_on_scroll = () => console.log('KEK');
+
+
+window.addEventListener('scroll', function() { 
+  scrollpos = window.scrollY;
+
+  if (navItems_height >= header_height) { add_class_on_scroll() }
+
+
+//   else { remove_class_on_scroll() }
+//   console.log(scrollpos)
+})
